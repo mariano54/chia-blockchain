d2138159c774f8e1bb2fad3a4b3e1624bd5d3fc3
Richard Kiss <him@richardkiss.com>
2020-05-18 16:10:39 -0700

    Refactor start_harvester to use start_service.

--- src/server/start_harvester.py
+++ src/server/start_harvester.py
@@ -1,6 +1,4 @@
 import asyncio
-import signal
-import logging
 
 try:
     import uvloop
@@ -8,55 +6,27 @@ except ImportError:
     uvloop = None
 
 from src.harvester import Harvester
-from src.server.outbound_message import NodeType
-from src.server.server import ChiaServer
 from src.util.config import load_config, load_config_cli
 from src.util.default_root import DEFAULT_ROOT_PATH
-from src.util.logging import initialize_logging
-from src.util.setproctitle import setproctitle
+
+from .start_service import async_start_service
 
 
 async def async_main():
+    service_type = "harvester"
     root_path = DEFAULT_ROOT_PATH
-    net_config = load_config(root_path, "config.yaml")
-    config = load_config_cli(root_path, "config.yaml", "harvester")
+    config = load_config_cli(root_path, "config.yaml", service_type)
+
     try:
         plot_config = load_config(root_path, "plots.yaml")
     except FileNotFoundError:
         raise RuntimeError("Plots not generated. Run chia-create-plots")
 
-    initialize_logging("Harvester %(name)-22s", config["logging"], root_path)
-    log = logging.getLogger(__name__)
-    setproctitle("chia_harvester")
-
-    harvester = await Harvester.create(config, plot_config)
-    ping_interval = net_config.get("ping_interval")
-    network_id = net_config.get("network_id")
-    assert ping_interval is not None
-    assert network_id is not None
-    server = ChiaServer(
-        config["port"],
-        harvester,
-        NodeType.HARVESTER,
-        ping_interval,
-        network_id,
-        DEFAULT_ROOT_PATH,
-        config,
+    api = await Harvester.create(config, plot_config)
+    await async_start_service(
+        api,
+        service_type,
     )
-
-    try:
-        asyncio.get_running_loop().add_signal_handler(signal.SIGINT, server.close_all)
-        asyncio.get_running_loop().add_signal_handler(signal.SIGTERM, server.close_all)
-    except NotImplementedError:
-        log.info("signal handlers unsupported")
-
-    harvester.set_server(server)
-    await asyncio.sleep(1)
-    harvester._start_bg_tasks()
-    await server.await_closed()
-    harvester._shutdown()
-    await harvester._await_shutdown()
-    log.info("Harvester fully closed.")
 
 
 def main():
