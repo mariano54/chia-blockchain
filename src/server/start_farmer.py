5ae66fbb95990576ddada324c5fc1b830763afff
Richard Kiss <him@richardkiss.com>
2020-05-18 19:41:53 -0700

    Factor out `_start_bg_tasks` from farmer.

--- src/server/start_farmer.py
+++ src/server/start_farmer.py
@@ -10,11 +10,35 @@ except ImportError:
 from src.farmer import Farmer
 from src.server.outbound_message import NodeType
 from src.server.server import ChiaServer
+from src.types.peer_info import PeerInfo
 from src.util.config import load_config, load_config_cli
 from src.util.default_root import DEFAULT_ROOT_PATH
 from src.cmds.init import check_keys
 from src.util.logging import initialize_logging
 from src.util.setproctitle import setproctitle
+
+
+def start_farmer_bg_task(server, peer_info, log):
+    """
+    Start a background task that checks connection and reconnects periodically to the full_node.
+    """
+
+    async def connection_check():
+        while True:
+            if server is not None:
+                full_node_retry = True
+
+                for connection in server.global_connections.get_connections():
+                    if connection.get_peer_info() == peer_info:
+                        full_node_retry = False
+
+                if full_node_retry:
+                    log.info(f"Reconnecting to full_node {peer_info}")
+                    if not await server.start_client(peer_info, None, auth=False):
+                        await asyncio.sleep(1)
+            await asyncio.sleep(30)
+
+    return asyncio.create_task(connection_check())
 
 
 async def async_main():
@@ -55,12 +79,15 @@ async def async_main():
 
     _ = await server.start_server(farmer._on_connect)
 
-    farmer.set_server(server)
     await asyncio.sleep(10)  # Allows full node to startup
-    farmer._start_bg_tasks()
+
+    peer_info = PeerInfo(
+        config["full_node_peer"]["host"], config["full_node_peer"]["port"]
+    )
+    farmer_bg_task = start_farmer_bg_task(server, peer_info, log)
 
     await server.await_closed()
-    farmer._shut_down = True
+    farmer_bg_task.cancel()
     log.info("Farmer fully closed.")
 
 
